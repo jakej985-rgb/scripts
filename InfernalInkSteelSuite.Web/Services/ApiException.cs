6194120dd48e84b83857ecac7fa1140af0281f6b
jakej985-rgb <jakej985@gmail.com>
2026-04-18 16:03:37 -0600

    add tattoo suite

--- InfernalInkSteelSuite.Web/Services/ApiException.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace InfernalInkSteelSuite.Web.Services;
-
-public class ApiException(HttpStatusCode statusCode, string content) : Exception(content)
-{
-    public HttpStatusCode StatusCode { get; } = statusCode;
-    public string Content { get; } = content;
-}
